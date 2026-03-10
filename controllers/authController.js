e42990d335c917ea0cad883abd32f75aad3e3b49
satyamrathod45 <satyamrathodi70@gmail.com>
2026-03-10 18:18:44 +0530

    ready for Deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -99,12 +99,12 @@ export const loginUser = async (req, res) => {
     );
 
     
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: false, 
-      sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000
-    });
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,  
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+})
 
     res.status(200).json({
       success: true,
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const server = http.createServer(app)
 // Socket Server
 export const io = new Server(server,{
   cors:{
-    origin:"http://localhost:5173",
+    origin:"https://satyamrathod45.github.io/",
     credentials:true
   }
 })
@@ -80,7 +80,7 @@ connectDB()
 
 app.use(
   cors({
-    origin:"http://localhost:5173",
+    origin:"http://satyamrathod45.github.io/",
     credentials:true
   })
 )
